01ec684db7cf3cc54a8a58c7b80f193bb91af745
Jonathas Santos <jonathaspe@hotmail.com>
2023-11-20 21:21:52 +0000

    Inclusão de novos recursos no método de carteira

--- carteira.rb
+++ carteira.rb
@@ -1,5 +1,10 @@
-nome = "Jonathas Santos"
-idade = 21
+puts "Qual é o seu nome?"
+#gets para aguardar que um dado seja digitado e o chomp para remover o pular linha
+nome = gets.chomp
+
+puts "Informe a sua idade:"
+#to_i para converter o resultado para um número inteiro
+idade = gets.chomp.to_i
 
 if(idade >= 18) 
     puts nome + ", você pode tirar carteira de motorista."
